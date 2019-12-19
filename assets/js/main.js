1f5f69e535f09012b493000b7ea1a63eeeb0d618
Joseph Lynn <josephlynn94@gmail.com>
2019-12-18 22:06:40 -0500

    changed the jquery to append html to page and added the ability to append the image

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,18 +34,22 @@ $("#searchBtn").on("click", function() {
 
   function currentConditions (response) {
 
+    // get the temperature and convert to fahrenheit 
     let tempF = (response.main.temp - 273.15) * 1.80 + 32;
     tempF = Math.floor(tempF);
 
-    const cardBody = $(".current-city-body");
+    // get and set the content 
+    const cardBody = $("#current-city");
     const city = $("#cityName").text(response.name);
-    const temp = $("<p>").addClass("current-city-text").text("Temperature: " + tempF + " °F");
-    const humid = $("<p>").addClass("current-city-text").text("Humidity: " + response.main.humidity + "%");
-    const wind = $("<p>").addClass("current-city-text").text("Wind Speed: " + response.wind.speed + " MPH");
+    const temp = $("#currentTemp").text("Temperature: " + tempF + " °F");
+    const humid = $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+    const wind = $("#currentWind").text("Wind Speed: " + response.wind.speed + " MPH");
+    const image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
 
     // add to page
+    city.append(image)
     cardBody.append(city, temp, humid, wind);
-
+   
   }
 
 
