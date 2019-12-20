9c6ebe1fed3b516c4b206f81fe8b17f821da5388
Joe Lynn <josephlynn94@gmail.com>
2019-12-19 19:53:31 -0500

    added margin to cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@ function getCurrentForecast () {
         let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
         let tempF = Math.floor(temp);
 
-        const card = $("<div>").addClass("card col-md-2 bg-primary text-white");
+        const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
         const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
         const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
         const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
